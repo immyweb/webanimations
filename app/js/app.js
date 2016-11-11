063974bebe1677dcf8925579b27f61c0ab40102d
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2016-11-11 16:38:20 +0000

    Removed header and footer

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import globalHeader from './components/global-header/index';
 import panelHero from './components/panel-hero/index';
 import panelVideo from './components/panel-video/index';
 import panelMessage from './components/panel-message/index';
@@ -10,7 +9,6 @@ import sectionWipes from './components/section-wipes/index';
 // run scripts on DOM ready
 $(document).ready(() => {
 
-    globalHeader.init();
     panelHero.init();
     panelVideo.init();
     panelMessage.init();
