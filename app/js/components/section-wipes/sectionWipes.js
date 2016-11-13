1c43b4213d56c22925ffc064b1e154f40acec194
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-11-13 22:27:55 +0000

    Tidy up

--- app/js/components/section-wipes/sectionWipes.js
+++ app/js/components/section-wipes/sectionWipes.js
@@ -14,22 +14,22 @@ export default class SectionWipes {
         const controller = new ScrollMagic.Controller();
 
         const wipeAnimation = new TimelineMax()
-                // in from left
-      			.fromTo('.section-wipes__panel.turqoise', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone })
-                // in from right
-      			.fromTo('.section-wipes__panel.green',    1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })
-                // in from top
-      			.fromTo('.section-wipes__panel.bordeaux', 1, { y: '-100%' }, { y: '0%', ease: Linear.easeNone });
+            // in from left
+  			.fromTo('.section-wipes__panel.turqoise', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone })
+            // in from right
+  			.fromTo('.section-wipes__panel.green',    1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })
+            // in from top
+  			.fromTo('.section-wipes__panel.bordeaux', 1, { y: '-100%' }, { y: '0%', ease: Linear.easeNone });
 
-            // create scene to pin and link animation
-    		new ScrollMagic.Scene({
-    				triggerElement: '#pinContainer',
-    				triggerHook: 'onLeave',
-    				duration: '300%'
-    			})
-    			.setPin('#pinContainer')
-    			.setTween(wipeAnimation)
-    			.addTo(controller);
+        // create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: '#pinContainer',
+				triggerHook: 'onLeave',
+				duration: '300%'
+			})
+			.setPin('#pinContainer')
+			.setTween(wipeAnimation)
+			.addTo(controller);
     }
 
 }
