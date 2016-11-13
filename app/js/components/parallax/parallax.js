36dbfc3d05c30a32f00cc7936e9f83730f536010
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-11-13 21:23:20 +0000

    Added new sections

--- app/js/components/parallax/parallax.js
+++ app/js/components/parallax/parallax.js
@@ -1,4 +1,3 @@
-import { TimelineMax } from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 
@@ -13,22 +12,22 @@ export default class Parallax {
     animateContent() {
         const controller = new ScrollMagic.Controller({
             globalSceneOptions: {
-                triggerHook: "onEnter",
-                duration: "200%"
+                triggerHook: 'onEnter',
+                duration: '200%'
             }
         });
 
         // build scenes
-      	new ScrollMagic.Scene({ triggerElement: "#parallax1" })
-      					.setTween("#parallax1 > div", { y: "80%", ease: Linear.easeNone })
+      	new ScrollMagic.Scene({ triggerElement: '#parallax1' })
+      					.setTween('#parallax1 > div', { y: '80%', ease: Linear.easeNone })
       					.addTo(controller);
 
-      	new ScrollMagic.Scene({ triggerElement: "#parallax2" })
-      					.setTween("#parallax2 > div", { y: "80%", ease: Linear.easeNone })
+      	new ScrollMagic.Scene({ triggerElement: '#parallax2' })
+      					.setTween('#parallax2 > div', { y: '80%', ease: Linear.easeNone })
       					.addTo(controller);
 
-      	new ScrollMagic.Scene({ triggerElement: "#parallax3" })
-      					.setTween("#parallax3 > div", { y: "80%", ease: Linear.easeNone })
+      	new ScrollMagic.Scene({ triggerElement: '#parallax3' })
+      					.setTween('#parallax3 > div', { y: '80%', ease: Linear.easeNone })
       					.addTo(controller);
     }
 
--- app/js/components/section-wipes/sectionWipes.js
+++ app/js/components/section-wipes/sectionWipes.js
@@ -14,9 +14,12 @@ export default class SectionWipes {
         const controller = new ScrollMagic.Controller();
 
         const wipeAnimation = new TimelineMax()
-      			.fromTo('.section-wipes__panel.turqoise', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone })  // in from left
-      			.fromTo('.section-wipes__panel.green',    1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })  // in from right
-      			.fromTo('.section-wipes__panel.bordeaux', 1, { y: '-100%' }, { y: '0%', ease: Linear.easeNone }); // in from top
+                // in from left
+      			.fromTo('.section-wipes__panel.turqoise', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone })
+                // in from right
+      			.fromTo('.section-wipes__panel.green',    1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })
+                // in from top
+      			.fromTo('.section-wipes__panel.bordeaux', 1, { y: '-100%' }, { y: '0%', ease: Linear.easeNone });
 
             // create scene to pin and link animation
     		new ScrollMagic.Scene({
