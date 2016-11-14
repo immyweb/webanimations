7e35eec742ed0a0d7313dea3ff2f5f58ed2a7135
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2016-11-14 14:07:53 +0000

    Image scroll prototype

--- app/js/components/pp-image-scroll/pp-image-scroll.js
+++ app/js/components/pp-image-scroll/pp-image-scroll.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import { TweenMax } from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 
@@ -5,12 +7,49 @@ export default class ImageScroll {
 
     init(element) {
         this.panel = element;
+		this.images = [];
 
-        this.animateContent();
+        this.createImageArray();
     }
 
+	createImageArray() {
+
+		for ( let i = 0; i < 209; i++) {
+			if ( i < 10 ) {
+				this.images.push( "../../images/pp-imagescroll/stoc000" + i + ".jpg" );
+			} else if ( i < 100 ) {
+				this.images.push( "../../images/pp-imagescroll/stoc00" + i + ".jpg" );
+			} else if ( i < 1000 ) {
+				this.images.push( "../../images/pp-imagescroll/stoc0" + i + ".jpg" );
+			}
+		}
+
+		this.animateContent();
+
+	}
+
     animateContent() {
-        console.log('image scroll initiated');
+
+		// TweenMax can tween any property of any object. We use this object to cycle through the array
+		const obj = { curImg: 0 };
+
+		const tween = TweenMax.to(obj, 0.5, {
+				curImg: this.images.length,		// animate propery curImg to number of images
+				roundProps: 'curImg',			// only integers so it can be used as an array index
+				repeat: 0,
+				immediateRender: true,			// load first image automatically
+				ease: Linear.easeNone,
+				onUpdate: () => {
+					$('.imagescroll__images').attr('src', this.images[obj.curImg]);
+				}
+			}
+		);
+
+		const controller = new ScrollMagic.Controller();
+
+		new ScrollMagic.Scene({ triggerElement: '.imagescroll', duration: 600 })
+			.setTween(tween)
+			.addTo(controller);
     }
 
 }
