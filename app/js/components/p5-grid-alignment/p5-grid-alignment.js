6693b94b2501a641192a052b32062f589869e0f9
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2016-11-16 16:23:39 +0000

    clean up

--- app/js/components/p5-grid-alignment/p5-grid-alignment.js
+++ app/js/components/p5-grid-alignment/p5-grid-alignment.js
@@ -1,4 +1,4 @@
-let max_distance = 500;
+// let max_distance = 500;
 
 const s = ( sk ) => {
 
@@ -18,8 +18,8 @@ const s = ( sk ) => {
         for ( let gridY = 0; gridY < sk.width; gridY+=25 ) {
             for ( let gridX = 0; gridX < sk.height; gridX+=25 ) {
 
-                let diameter = sk.dist(sk.mouseX, sk.mouseY, gridX, gridY);
-                diameter = (diameter/max_distance) * 40;
+                // let diameter = sk.dist(sk.mouseX, sk.mouseY, gridX, gridY);
+                // diameter = (diameter/max_distance) * 40;
 
 				let ranSat = sk.random(70, 100);
 				let ranAlpha = sk.random(30, 70);
