a094d81d2b9741e0a9d57d1b783a6853282cd245
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-11-19 12:02:50 +0000

    Created Walker class

---
+++ app/js/components/nature-of-code/classes/walker.js
@@ -0,0 +1,31 @@
+export default class Walker {
+
+	constructor(p5) {
+		this.p5 = p5;
+		this.x = this.p5.width/2;
+		this.y = this.p5.height/2;
+	}
+
+	render() {
+		this.p5.stroke(0, 0, 100);
+		this.p5.point(this.x, this.y);
+	}
+
+	step() {
+		let choice = Math.round(this.p5.random(4));
+		// let choice = Math.round(this.p5.randomGaussian(2, 1));
+
+		if (choice === 0) {
+	     	this.x++;
+	    } else if (choice === 1) {
+	     	this.x--;
+	    } else if (choice === 2) {
+	     	this.y++;
+	    } else {
+	     	this.y--;
+	    }
+
+		this.x = this.p5.constrain(this.x, 0, this.p5.width-1);
+    	this.y = this.p5.constrain(this.y, 0, this.p5.height-1);
+	}
+}
--- app/js/components/nature-of-code/random-walker.js
+++ app/js/components/nature-of-code/random-walker.js
@@ -1,3 +1,5 @@
+import Walker from './classes/walker';
+
 let walker;
 
 const s = ( p5 ) => {
@@ -14,37 +16,5 @@ const s = ( p5 ) => {
 		walker.render();
     };
 };
-
-class Walker {
-
-	constructor(p5) {
-		this.p5 = p5;
-		this.x = this.p5.width/2;
-		this.y = this.p5.height/2;
-	}
-
-	render() {
-		this.p5.stroke(0, 0, 100);
-		this.p5.point(this.x, this.y);
-	}
-
-	step() {
-		let choice = Math.round(this.p5.random(4));
-		// let choice = Math.round(this.p5.randomGaussian(2, 1));
-
-		if (choice === 0) {
-	     	this.x++;
-	    } else if (choice === 1) {
-	     	this.x--;
-	    } else if (choice === 2) {
-	     	this.y++;
-	    } else {
-	     	this.y--;
-	    }
-
-		this.x = this.p5.constrain(this.x, 0, this.p5.width-1);
-    	this.y = this.p5.constrain(this.y, 0, this.p5.height-1);
-	}
-}
 
 export default s;
