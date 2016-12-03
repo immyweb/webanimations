8ea1b4b14525d985e90b62920e03562f814729db
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-12-03 14:17:03 +0000

    navigation setup

--- app/js/components/greensock/slideshow.js
+++ app/js/components/greensock/slideshow.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import { TimelineMax } from 'gsap';
+import { TweenLite } from 'gsap';
 import CSSRulePlugin from 'gsap/src/uncompressed/plugins/CSSRulePlugin.js';
 
 export default class GsapSlideshow {
@@ -7,6 +7,47 @@ export default class GsapSlideshow {
     init(element) {
         this.panel = element;
 
-		console.log(this.panel);
+		// Variables
+		let sectionForm,
+			$slide = this.panel.find('.slide'),
+			$slideActive = this.panel.find('.slide.active'),
+			$navLink = this.panel.find('.nav a'),
+			$wrapper = $('.slidesContainer-wrapper');
+
+		// Init function
+		function init() {
+			// Set active slide visible
+			TweenLite.set($slideActive, { y: '0%' });
+
+			// Fade slides in
+			TweenLite.set($wrapper, { className: '-=loading' });
+		}
+		init();
+
+		// Navigation click
+		$navLink.on('click', (e) => {
+
+			let sectionFrom = this.panel.find('.slide.active'),
+				sectionToID = $(e.target).attr('href'),
+				sectionTo = $('div'+sectionToID);
+
+			if ( sectionFrom.attr('id') !== sectionTo.attr('id') ) {
+				scrollToSection(sectionFrom, sectionTo);
+			}
+
+
+			e.preventDefault();
+		});
+
+		function scrollToSection(sectionFrom, sectionTo) {
+
+			if ( sectionFrom.index() < sectionTo.index() ) {
+				console.log('going down');
+			} else {
+				console.log('going up');
+			}
+
+		}
+
 	}
 }
