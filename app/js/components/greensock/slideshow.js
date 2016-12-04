06b0eba10ba17512f987035d883d98cfc348381d
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-12-04 17:50:52 +0000

    Gallery slideshow

--- app/js/components/greensock/slideshow.js
+++ app/js/components/greensock/slideshow.js
@@ -13,6 +13,7 @@ export default class GsapSlideshow {
 			$slideActive = this.panel.find('.slide.active'),
 			$navLink = this.panel.find('.nav a'),
 			$navLi = this.panel.find('.nav li'),
+			$nav = this.panel.find('.nav'),
 			$wrapper = $('.slidesContainer-wrapper');
 
 		// Init function
@@ -55,27 +56,55 @@ export default class GsapSlideshow {
 
 				// console.log('going down');
 				tlNext
+					// .set($wrapper, { className: '+=is-animating' })
+					// .to(sectionFrom, 1.2, { x: '-=100%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, '0')
+					// .to(bcgFrom, 1.2, { x: '30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .from(bcg, 1.2, { x: '-30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .from(heading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=1')
+					// .from(subHeading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=0.6')
+					// .set($wrapper, { className: '-=is-animating' });
 					.set($wrapper, { className: '+=is-animating' })
-					.to(sectionFrom, 1.2, { x: '-=100%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, '0')
-					.to(bcgFrom, 1.2, { x: '30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.from(bcg, 1.2, { x: '-30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.from(heading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=1')
-					.from(subHeading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=0.6')
+					.set(sectionTo, { scale: 0.9 })
+					.add('out')
+					.to($nav, 0.3, { y: '10px', autoAlpha: 0, ease: Power4.easeInOut }, 'out')
+					.to(sectionFrom, 0.3, { scale: 0.9, transformOrgin: 'center center', ease: Power4.easeInOut }, 'out')
+					.add('across')
+					.to(sectionFrom, 1.2, { x: '-=100%', ease: Power4.easeInOut, clearProps: 'all' }, 'out-=0.1')
+					.to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, 'out-=0.1')
+					.add('in')
+					.to(sectionTo, 0.3, { scale: 1, ease: Power4.easeInOut }, 'in-=0.4')
+					.from(heading, 0.3, { autoAlpha: 0, y: '-15px', ease: Power4.easeInOut }, 'in')
+					.from(subHeading, 0.3, { autoAlpha: 0, y: '-15px', ease: Power4.easeInOut }, 'in+=0.1')
+					.to($nav, 0.3, { autoAlpha: 1, y: '0', ease: Power4.easeInOut }, 'in+=0.2')
 					.set($wrapper, { className: '-=is-animating' });
 
 			} else {
 
 				// console.log('going up');
 				tlPrev
+					// .set($wrapper, { className: '+=is-animating' })
+					// .set(sectionTo, { x: '-100%' })
+					// .to(sectionFrom, 1.2, { x: '100%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, '0')
+					// .to(bcgFrom, 1.2, { x: '-30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .from(bcg, 1.2, { x: '30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
+					// .from(heading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=1')
+					// .from(subHeading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=0.6')
+					// .set($wrapper, { className: '-=is-animating' });
 					.set($wrapper, { className: '+=is-animating' })
-					.set(sectionTo, { x: '-100%' })
-					.to(sectionFrom, 1.2, { x: '100%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, '0')
-					.to(bcgFrom, 1.2, { x: '-30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.from(bcg, 1.2, { x: '30%', ease: Power4.easeInOut, clearProps: 'all' }, '0')
-					.from(heading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=1')
-					.from(subHeading, 0.7, {autoAlpha: 0, x: 40, ease: Power4.easeInOut }, '-=0.6')
+					.set(sectionTo, { x: '-100%', scale: 0.9 })
+					.add('out')
+					.to($nav, 0.3, { y: '10px', autoAlpha: 0, ease: Power4.easeInOut }, 'out')
+					.to(sectionFrom, 0.3, { scale: 0.9, transformOrgin: 'center center', ease: Power4.easeInOut }, 'out')
+					.add('across')
+					.to(sectionFrom, 1.2, { x: '100%', ease: Power4.easeInOut, clearProps: 'all' }, 'out-=0.1')
+					.to(sectionTo, 1.2, { x: '0%', ease: Power4.easeInOut }, 'out-=0.1')
+					.add('in')
+					.to(sectionTo, 0.3, { scale: 1, ease: Power4.easeInOut }, 'in-=0.4')
+					.from(heading, 0.3, { autoAlpha: 0, y: '-15px', ease: Power4.easeInOut }, 'in')
+					.from(subHeading, 0.3, { autoAlpha: 0, y: '-15px', ease: Power4.easeInOut }, 'in+=0.1')
+					.to($nav, 0.3, { autoAlpha: 1, y: '0', ease: Power4.easeInOut }, 'in+=0.2')
 					.set($wrapper, { className: '-=is-animating' });
 
 			}
