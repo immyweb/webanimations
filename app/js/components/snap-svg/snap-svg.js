e792f3306553a6912c8cb03086d112e55cf24cfb
Imran Oozeerally 7 <ioozeerally7@sapient.com>
2016-12-06 23:27:27 +0000

    Grid dropdown and bounce

--- app/js/components/snap-svg/snap-svg.js
+++ app/js/components/snap-svg/snap-svg.js
@@ -1,17 +1,20 @@
+import $ from 'jquery';
 import Snap from 'snapsvg';
-// import TweenMax from 'gsap';
+import { TweenMax, TimelineMax } from 'gsap';
 import colourJson from '../../../data/colours.json';
 import { random } from 'lodash';
 
 const colors = colourJson;
 const w = window.innerWidth;
-// const h = window.innerHeight;
+const h = window.innerHeight;
 
 const s = Snap('#snap-svg');
+let index = 0;
 
 export default class SnapSvg {
 
-    init() {
+    init(element) {
+		this.container = element;
 		this.draw();
     }
 
@@ -23,14 +26,16 @@ export default class SnapSvg {
 			// let layer1 = poly1.select('.cls-1');
 			// let layer2 = poly1.select('.cls-2');
 
-			for ( let gridX = 0; gridX < w; gridX+=57 ) {
-				for ( let gridY = 0; gridY < 300; gridY+=51 ) {
+			for ( let gridY = 0; gridY < 300; gridY+=51 ) {
+				for ( let gridX = 0; gridX < w; gridX+=57 ) {
 
+					index = index + 1;
 					let randomIndex = random(colors.length-1);
 					let randomAlpha = random(0.25, 0.75);
 
 					const clone = poly1.clone();
 					clone.transform(new Snap.Matrix().translate(gridX, gridY));
+					clone.addClass(`shape shape${index}`);
 					clone.attr({
 					    fill: colors[randomIndex],
 					    stroke: colors[randomIndex],
@@ -47,7 +52,19 @@ export default class SnapSvg {
 	}
 
 	animate() {
-		console.log(s.select('g'));
+		let svgGroup = s.selectAll('g'),
+			$shapes = this.container.find('.shape');
+
+		const tlRainShapes = new TimelineMax();
+
+		tlRainShapes
+			.staggerTo($shapes, 0.75, { y: h-100, ease: Power4.easeIn, onComplete: bounceUp, onCompleteParams: ["{self}"] }, -0.1)
+		;
+
+		function bounceUp(tween) {
+			// console.log(tween.target);
+			TweenMax.to(tween.target, 0.5, { y: '-=100px' })
+		}
 	}
 
 }
