4386da8348ca72214615a68ca11bfc2f41ab1f4f
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-12-14 17:57:21 +0000

    Scene 3 - Triggers down

--- app/js/components/scrollmagic/one-pager.js
+++ app/js/components/scrollmagic/one-pager.js
@@ -9,7 +9,22 @@ export default class OnePager {
     init(element) {
         this.panel = element;
 
-		const controller = new ScrollMagic.Controller();
+		let controller,
+			$navItems = $('.nav-items li').not('.active'),
+			$navTrigger = $('.nav-trigger'),
+			getTriggersDown = $('.slide-pos'),
+			triggersDown = [];
+
+
+		// Triggers on the way down
+		$.each(getTriggersDown, (key, value) => {
+
+			let id = '#' + value.id;
+			triggersDown.push(id);
+
+		});
+
+		controller = new ScrollMagic.Controller();
 
 		// Scene 1  - pin our main section
 		const pinScene01 = new ScrollMagic.Scene({
@@ -20,7 +35,51 @@ export default class OnePager {
 
 		pinScene01
 			.setPin('#main .pin-wrapper', { pushFollowers: false })
-			.addTo(controller)
-		;
+			.addTo(controller);
+
+
+		// Navigation timeline
+		let navTl = new TimelineMax();
+
+		// Move navigation right by 26px for each item
+		$navItems.each((index, item) => {
+			let slideHREF = $(item).find('a').attr('href'),
+				slideID = slideHREF.substr(slideHREF.length -7),
+				moveNav = TweenMax.to($('.nav-active'), 1, { x: '+=26', ease: Linear.easeNone });
+
+			// Add individual tweens to the timeline
+			navTl.add(moveNav, slideID);
+		});
+
+		// Scene 2 - move navigation
+		const navScene = new ScrollMagic.Scene({
+			triggerElement: $navTrigger,
+			duration: '800%'
+		});
+
+		navScene
+			.setTween(navTl)
+			.addTo(controller);
+
+		// Scene 3 - trigger the right animation on the way DOWN
+		triggersDown.forEach((triggerDown, index) => {
+
+			let triggerTransitionToNext = new ScrollMagic.Scene({
+				triggerElement: triggerDown,
+				triggerHook: 0.6
+			});
+
+			triggerTransitionToNext
+				.on('enter', (e) => {
+					console.log('crossfade to next ' +triggerDown);
+				})
+				.addIndicators({
+					name: 'triggerDown',
+					indent: 520,
+					colorStart: 'yellow',
+					colorTrigger: 'yellow'
+				})
+				.addTo(controller);
+		});
 	}
 }
